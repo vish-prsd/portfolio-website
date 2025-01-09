e2534a009bace0b1c198ff738d0eb2855aadca92
vish-prsd <psvishnu286@gmail.com>
2025-01-09 21:24:51 +0530

    styling tweeks

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ function App() {
           <Route path = "/" element={
             <>
             <About />
+            <Education />
+            <Certifications />
             <Projects />
             <Contact />
             </>
